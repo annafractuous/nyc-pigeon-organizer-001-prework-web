1dfcfe599fecc834f850eff95974e3e2937e15eb
Anna Rasshivkina <anna.nigma@yahoo.com>
2015-11-17 01:06:11 -0500

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  sorted_pigeons = {}
+  
+  data.each do |attrb, qualities|
+    qualities.each do |quality, pigeons|
+      pigeons.each do |pigeon|
+        if sorted_pigeons.has_key?(pigeon) == false
+          sorted_pigeons[pigeon] = { color: [], gender: [], lives: [] }
+          sorted_pigeons[pigeon][attrb] << quality.to_s
+        else
+          sorted_pigeons[pigeon][attrb] << quality.to_s
+        end
+      end
+    end
+  end
+
+  sorted_pigeons
+  
 end
