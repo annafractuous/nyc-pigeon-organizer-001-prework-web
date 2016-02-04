abdd12e30cdee860bd17b6367fb6cb34768a0bd6
Anna Rasshivkina <anna.nigma@yahoo.com>
2016-02-04 11:16:13 -0500

    Abstract methods further

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,24 +19,52 @@ pigeon_data = {
   }
 }
 
-def empty_pigeon_hash(pigeon_data)
-  names = pigeon_data[:gender].values.flatten
-  pigeons = names.each_with_object({}) { |name, hash| hash[name] = { color: [], gender: [], lives: [] } }
+def names(pigeon_data)
+  names = pigeon_data.fetch(:gender).values.flatten
+end
+
+def properties(pigeon_data)
+  pigeon_data.keys
+end
+
+def initial_structure(pigeon_names)
+  pigeons = pigeon_names.each_with_object({}) { |name, hash| hash[name] = { color: [], gender: [], lives: [] } }
+end
+
+def populate(initial_structure, names, data)
+  names.each_with_object(initial_structure) do |name, initial_structure|
+    properties(data).each do |property|
+      initial_structure[name][property] = find_pigeon_value(name, data, property).compact
+    end
+  end
+end
+
+def find_pigeon_value(name, data, value)
+  data.fetch(value).collect { |characteristic, pigeons| characteristic.to_s if pigeons.include?(name) }
 end
 
 def nyc_pigeon_organizer(pigeon_data)
-  sorted_pigeons = empty_pigeon_hash(pigeon_data)
-
-  # iterate into lowest level of unsorted pigeon data: pigeon names
-  pigeon_data.each do |category, options|
-    options.each do |option, pigeons|
-      pigeons.each { |pigeon| sorted_pigeons[pigeon][category] << option.to_s }
-    end
-  end
-
-  sorted_pigeons
+  names = names(pigeon_data)
+  empty_hash = initial_structure(names)
+  sorted_pigeons = populate(empty_hash, names, pigeon_data)
 end
 
+# REHASHED TAKE 1
+# def nyc_pigeon_organizer(pigeon_data)
+#   pigeon_names = names(pigeon_data)
+#   sorted_pigeons = initial_structure(pigeon_names)
+#
+#   # iterate into lowest level of unsorted pigeon data: pigeon names
+#   pigeon_data.each do |category, options|
+#     options.each do |option, pigeons|
+#       pigeons.each { |pigeon| sorted_pigeons[pigeon][category] << option.to_s }
+#     end
+#   end
+#
+#   sorted_pigeons
+# end
+
+# ORIGINAL ANSWER
   # sorted_pigeons = {}
   #
   # data.each do |attrb, qualities|
